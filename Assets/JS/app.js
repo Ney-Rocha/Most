a264dd6902a73b6179100e54b824369ab94b2857
Ney Rocha <neyrocha.brasil@gmail.com>
2023-11-02 00:00:00 -0300

    correção do form em teste, antes da entrega

--- Assets/JS/app.js
+++ Assets/JS/app.js
@@ -8,70 +8,74 @@ class FormSubmit {
         }
         this.sendForm = this.sendForm.bind(this);
 
-        this.formButton.addEventListener("click", this.validateAndSubmit.bind(this));
+        if (this.formButton) {
+            this.formButton.addEventListener("click", this.validateAndSubmit.bind(this));
+        }
     }
 
     displaySuccess() {
-        this.form.innerHTML = this.settings.success;
+        if (this.form) {
+            this.form.innerHTML = this.settings.success;
+        }
     }
 
     displayError(errorMessage, field) {
-        // Exibe a mensagem de erro diretamente abaixo do campo com erro.
-        const errorElement = document.createElement("span");
-        errorElement.className = "error-message  d-flex ml-20 strong text-danger mt-1";
-        errorElement.innerText = errorMessage;
-
-        // Insere a mensagem de erro após o campo com erro.
-        field.parentNode.insertBefore(errorElement, field.nextSibling);
+        if (field && field.parentNode) {
+            const errorElement = document.createElement("span");
+            errorElement.className = "error-message text-danger mt-2";
+            errorElement.innerText = errorMessage;
+            field.parentNode.insertBefore(errorElement, field.nextSibling);
+        }
     }
 
     clearErrors() {
-        // Remove todas as mensagens de erro.
         const errorMessages = this.form.querySelectorAll(".error-message");
         errorMessages.forEach((error) => error.remove());
     }
 
     getFormObject() {
         const formObject = {};
-        const fields = this.form.querySelectorAll("[name]");
-
-        fields.forEach((field) => {
-            
-            if (field.type === "checkbox") {
-                formObject[field.getAttribute("name")] = field.checked ? "sim" : "não";
-            } else if (field.tagName === "TEXTAREA") {
-                formObject[field.getAttribute("name")] = field.value;
-            }  else {
-                formObject[field.getAttribute("name")] = field.value;
-            }
-        });
-
-        formObject['mensagem'] = this.form.querySelector("[name='mensagem']").value;
-
+        if (this.form) {
+            const fields = this.form.querySelectorAll("[name]");
+            fields.forEach((field) => {
+                if (field.type === "checkbox") {
+                    formObject[field.getAttribute("name")] = field.checked ? "sim" : "não";
+                } else if (field.tagName === "TEXTAREA") {
+                    formObject[field.getAttribute("name")] = field.value;
+                } else {
+                    formObject[field.getAttribute("name")] = field.value;
+                }
+            });
+        }
         return formObject;
     }
 
     onSubmission(event) {
-        event.preventDefault();
-        event.target.disabled = true;
-        event.target.innerText = "Enviando...";
+        if (event) {
+            event.preventDefault();
+            if (event.target) {
+                event.target.disabled = true;
+                event.target.innerText = "Enviando...";
+            }
+        }
     }
 
     async sendForm(e) {
         try {
             this.onSubmission(e);
-            await fetch(this.url, {
-                method: "POST",
-                headers: {
-                    "Content-Type": "application/json",
-                    Accept: "application/json",
-                },
-                body: JSON.stringify(this.getFormObject()),
-            });
-
+            if (this.url) {
+                await fetch(this.url, {
+                    method: "POST",
+                    headers: {
+                        "Content-Type": "application/json",
+                        Accept: "application/json",
+                    },
+                    body: JSON.stringify(this.getFormObject()),
+                });
+            }
             this.displaySuccess();
             window.location.href = "/simulador5.html";
-
+            
         } catch (error) {
             this.displayError("Não foi possível enviar o formulário. Por favor, tente novamente.");
             throw new Error(error);
@@ -79,66 +83,49 @@ class FormSubmit {
     }
 
     validateAndSubmit(e) {
-        e.preventDefault();
-        this.clearErrors(); // Limpa mensagens de erro anteriores.
-
-        if (this.validateForm()) {
-            this.onSubmission(e);
-            this.sendForm(e);
+        if (this.form) {
+            this.clearErrors();
+            if (this.validateForm()) {
+                this.onSubmission(e);
+                this.sendForm(e);
+            }
         }
     }
 
     validateForm() {
-        const fields = this.form.querySelectorAll("[name]");
-        let isValid = true; // Assume que o formulário é válido até encontrar um erro.
-
-        for (const field of fields) {
-            if (field.hasAttribute("required") && !field.value.trim()) {
-                this.displayError(`Campo '${field.getAttribute("name")}' é obrigatório.`, field);
-                isValid = false;
-            }
-            if (field.hasAttribute("type")) {
-                const fieldType = field.getAttribute("type");
-                if (fieldType === "email" && field.value.trim() && !this.validateEmail(field.value)) {
-                    this.displayError(`Campo '${field.getAttribute("name")}' não é um e-mail válido.`, field);
+        if (this.form) {
+            const fields = this.form.querySelectorAll("[name]");
+            let isValid = true;
+            for (const field of fields) {
+                if (field.hasAttribute("required") && !field.value.trim()) {
+                    this.displayError(`Campo '${field.getAttribute("name")}' é obrigatório.`, field);
                     isValid = false;
                 }
-                // Adicione mais verificações de tipo de campo, se necessário.
+                if (field.hasAttribute("type")) {
+                    const fieldType = field.getAttribute("type");
+                    if (fieldType === "email" && field.value.trim() && !this.validateEmail(field.value)) {
+                        this.displayError(`Campo '${field.getAttribute("name")}' não é um e-mail válido.`, field);
+                        isValid = false;
+                    }
+                    if (fieldType === "tel" && !this.validatePhoneNumber(field.value)) {
+                        this.displayError(`Campo '${field.getAttribute("name")}' não é um número de telefone válido.`, field);
+                        isValid = false;
+                    }
+                }
             }
+            return isValid;
         }
-        return isValid;
+        return false;
     }
-    
+
     validatePhoneNumber(phoneNumber) {
-        // Remove espaços em branco e caracteres especiais do número de telefone.
         const cleanedNumber = phoneNumber.replace(/\D/g, '');
-
-        // Verifica se o número de telefone é um celular com 11 dígitos ou um telefone fixo com 10 dígitos.
-        const isPhoneNumberValid = /^[0-9]{10,11}$/.test(cleanedNumber);
-
-        if (!isPhoneNumberValid) {
-            return false;
-        }
-
-        // Adiciona a máscara com DDD (xx) xxxxx-xxxx ou (xx) xxxx-xxxx.
-        const maskedPhoneNumber = cleanedNumber.length === 11
-            ? `(${cleanedNumber.slice(0, 2)}) ${cleanedNumber.slice(2, 7)}-${cleanedNumber.slice(7)}`
-            : `(${cleanedNumber.slice(0, 2)}) ${cleanedNumber.slice(2, 6)}-${cleanedNumber.slice(6)}`;
-
-        // Atualiza o valor do campo com a versão formatada.
-        this.form.querySelector("[name='telefone']").value = maskedPhoneNumber;
-
-        return true;
+        return /^[0-9]{10,11}$/.test(cleanedNumber);
     }
 
     validateEmail(email) {
         const emailRegex = /^[^\s@]+@[^\s@]+\.[^\s@]+$/;
         return emailRegex.test(email);
-    }
-
-    init() {
-        if (this.form)
-            return this;
     }
 }
 
@@ -146,7 +133,7 @@ const formSubmit = new FormSubmit({
     form: "[data-form]",
     button: "[data-button]",
     success: "<h1 class='text-success'>Mensagem enviada</h1>",
-    error: "<h1 class='text-danger'>Não foi possível enviar sua mensagem!</h1>",
+    error: "<h1 class='text-secondary'>Não foi possível enviar sua mensagem!</h1>",
 });
 
-formSubmit.init();
+// formSubmit.init();
